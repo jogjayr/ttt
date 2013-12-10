b2d9bdbc1bd3df80e4a91837f96f96c1d7c3bc93
Jayraj Jog <jayraj@bittorrent.com>
2013-12-09 22:51:41 -0800

    More comments explaining board-state

--- src/tictactoe/logic.clj
+++ src/tictactoe/logic.clj
@@ -7,14 +7,13 @@
 ;    6  |  7  |  8
 ;       |     |
 
-
+;; board state is saved as a list of vectors. each item represents one turn played
+;; ["" "x" "" "" "" "" "" "" ""] represents a board with an "x" on square 1
 (def board-state [(vec (repeat 9 ""))])
 
 ;; move-char - "x" or "o"
 ;; move-square - squares are numbered from 0-8 as shown above
 ;; @return - the next move (if the move was played by the player)
-
-
 (defn compute-next-move [move-char move-square]
 	(let [current-state (last board-state)]
 		;; create a new state vector with the given move
