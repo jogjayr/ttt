e16484c13fc154b17963a604f4fc13fd931035a2
Jayraj Jog <jayraj@bittorrent.com>
2013-12-12 19:36:07 -0800

    Convert tabs to spaces. This will never happen again because I have made it clear to Sublime that it is not to happen again ever

--- src/tictactoe/logic.clj
+++ src/tictactoe/logic.clj
@@ -13,40 +13,40 @@
 
 ;;count the number occurrences of move in a given-state
 (defn move-count [move given-state]
-	(count (filter #(= move %) given-state)))
+  (count (filter #(= move %) given-state)))
 
 ;; move-char - "x" or "o"
 ;; move-square - squares are numbered from 0-8 as shown above
 ;; @return - the next move (if the move was played by the player)
 (defn compute-next-move [move-char move-square]
-	{:pre [(and (<= move-square 8) (>= move-square 0))] }
+  {:pre [(and (<= move-square 8) (>= move-square 0))] }
 
-	(let [current-state (last board-state)
+  (let [current-state (last board-state)
 
-				;; create a new state vector with the given move
-				newest-state  (assoc current-state move-square move-char)
+        ;; create a new state vector with the given move
+        newest-state  (assoc current-state move-square move-char)
 
-				;; get the number of x-es in the newest state so we can test 
-				x-es-in-new (move-count "x" newest-state)
+        ;; get the number of x-es in the newest state so we can test 
+        x-es-in-new (move-count "x" newest-state)
 
-				;;if it's out of whack with the number of o-s
-				o-s-in-new (move-count "o" newest-state)]
-		
-		;;check that a move isn't being played on an already-played square
-		(if (some #(= newest-state %) board-state)
-			(throw (Exception. "Move has already been played")))
-		
-		;;check that the number of x-es is exactly 1 greater than the number of o-s
-		;;because x plays first and compute next move is always called on x (for now)
-		(if (> (- x-es-in-new o-s-in-new) 1)
-			(throw (Exception. "Board state is fucked up")))
+        ;;if it's out of whack with the number of o-s
+        o-s-in-new (move-count "o" newest-state)]
+    
+    ;;check that a move isn't being played on an already-played square
+    (if (some #(= newest-state %) board-state)
+      (throw (Exception. "Move has already been played")))
+    
+    ;;check that the number of x-es is exactly 1 greater than the number of o-s
+    ;;because x plays first and compute next move is always called on x (for now)
+    (if (> (- x-es-in-new o-s-in-new) 1)
+      (throw (Exception. "Board state is fucked up")))
 
-		;; append the new state vector to the board-state
-		(def board-state (conj board-state newest-state))
+    ;; append the new state vector to the board-state
+    (def board-state (conj board-state newest-state))
 
-		;; return the board state
-		;; TODO: make this return the next move (computer's move)
-		board-state))
+    ;; return the board state
+    ;; TODO: make this return the next move (computer's move)
+    board-state))
 
 ;; check the board state and return name of winner
 ;; false otherwise
--- test/tictactoe/logic_test.clj
+++ test/tictactoe/logic_test.clj
@@ -1,31 +1,31 @@
 (ns tictactoe.logic-test
   (:require [clojure.test :refer :all] 
-			[tictactoe.logic :refer :all]))
+      [tictactoe.logic :refer :all]))
 
 (deftest next-move-insert
   (testing "Next move is inserted into board state"
-  	(compute-next-move "x" 8)
+    (compute-next-move "x" 8)
     (let [last-move (last board-state)]
       (is (= "x" (last-move 8))))))
 
 (deftest play-same-square
-	(testing "Throws an Exception if a previously played square is played again"
-		(is (thrown? Exception (compute-next-move "x" 8) 0)))
-	(testing "Throws an Exception if two x-es in a row are played"
-		(is (thrown? Exception (compute-next-move "x" 6)))))
+  (testing "Throws an Exception if a previously played square is played again"
+    (is (thrown? Exception (compute-next-move "x" 8) 0)))
+  (testing "Throws an Exception if two x-es in a row are played"
+    (is (thrown? Exception (compute-next-move "x" 6)))))
 
 (deftest move-square-limits
-	(testing "Rejects a move square greater than 8"
-		(is (thrown? AssertionError (compute-next-move "x" 9))))
-	(testing "Accepts a move square less than 8 and greater than 0"
-		(compute-next-move "o" 5)
-		(is (= "o" ((last board-state) 5))))
-	(testing "Rejects move square less than 0"
-		(is (thrown? AssertionError (compute-next-move "x" -1)))))
+  (testing "Rejects a move square greater than 8"
+    (is (thrown? AssertionError (compute-next-move "x" 9))))
+  (testing "Accepts a move square less than 8 and greater than 0"
+    (compute-next-move "o" 5)
+    (is (= "o" ((last board-state) 5))))
+  (testing "Rejects move square less than 0"
+    (is (thrown? AssertionError (compute-next-move "x" -1)))))
 
 (deftest move-occurrence-counts
-	(testing "Returns 0 if no occurrence of moves in the state"
-		(is (= 0 (move-count "y" board-state))))
-	(testing "Returns 2 if 2 occurrences of 'x' in the state"
-		(is (= 2 (move-count "x" ["x" "o" "o" "" "" "x" "" ""])))))
+  (testing "Returns 0 if no occurrence of moves in the state"
+    (is (= 0 (move-count "y" board-state))))
+  (testing "Returns 2 if 2 occurrences of 'x' in the state"
+    (is (= 2 (move-count "x" ["x" "o" "o" "" "" "x" "" ""])))))
 
