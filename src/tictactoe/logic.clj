031f69085cff40f01ca223764b7b40e43339de80
Jayraj Jog <jayraj@bittorrent.com>
2013-12-13 09:17:39 -0800

    Refactored compute-next-move so that it's not modifying the board state (like a good side-effect-free function) This also has the happy effect of making my tests simpler; there's (mostly) no global board-state to keep track of

--- src/tictactoe/logic.clj
+++ src/tictactoe/logic.clj
@@ -16,11 +16,11 @@
   (count (filter #(= move %) given-state)))
 
 (defn is-valid-move? 
-  "Returns true if the move is valid, false and reason if it's not valid"
-  [move-char move-square]
+  "Returns true if the move is valid, throws an exception if not"
+  [move-char move-square board]
   (if (or (> move-square 8) (< move-square 0))
     (throw  (AssertionError. "Invalid move square"))
-    (let [current-state (last board-state)
+    (let [current-state (last board)
 
           ;; create a new state vector with the given move
           newest-state  (assoc current-state move-square move-char)
@@ -32,28 +32,54 @@
           o-s-in-new (move-count "o" newest-state)]
     
     ;;check that a move isn't being played on an already-played square
-    (if (some #(= newest-state %) board-state)
+    (if (some #(= newest-state %) board)
       (throw (Exception. "Move has already been played"))
     
       ;;check that the number of x-es is exactly 1 greater than the number of o-s
       ;;because x plays first and compute next move is always called on x (for now)
       (if (> (- x-es-in-new o-s-in-new) 1)
         (throw (Exception. "Board state is fucked up"))
-        
+
         true)))))
 
 ;; move-char - "x" or "o"
 ;; move-square - squares are numbered from 0-8 as shown above
 ;; @return - the next move (if the move was played by the player)
-(defn compute-next-move [move-char move-square]
-  {:pre [(is-valid-move? move-char move-square)] }
-    (let [newest-state (assoc (last board-state) move-square move-char)]
-      ;; append the new state vector to the board-state
-      (def board-state (conj board-state newest-state))
+(defn compute-next-move [move-char move-square board]
 
-      ;; return the board state
-      ;; TODO: make this return the next move (computer's move)
-      board-state))
+  {:pre [(is-valid-move? move-char move-square board)] }
+
+    (let [newest-move (assoc (last board) move-square move-char)
+
+          ;;find the blank squares on the board
+          blank-squares (keep-indexed (fn [idx value] (if (= value "") idx nil )) newest-move)]
+
+      (print blank-squares)
+
+      ;; want to make a game engine that will always at least draw
+      ;; so it will look to play the 4 square 1st, then 0, 2, 6, 8 (the corners)
+      ;; with equal priority, then 1, 3, 5, 7 with also equal priority
+
+      ;; 1. check if 4 is played
+      (if (some #{4} blank-squares)
+        (do
+          (print "four isn't played")
+          (assoc newest-move 4 "o"))
+
+        ;; 2. if it has been played (by either player), find the first of 0, 2, 6, 8 that
+        ;;    haven't been played, and do it
+        (let [free-corner (some #{0 2 6 8} blank-squares)]
+          (if free-corner
+            (do 
+              (print free-corner " is a free corner")
+              (assoc newest-move free-corner "o"))
+          ;; 3. if no corner square is free, find the first of 1, 3, 5, 7 that hasn't been played
+            (let [free-side (some #{1 3 5 7} blank-squares)]
+              (if free-side
+                (do 
+                  (print free-side " is a free side")
+                  (assoc newest-move free-side "o"))
+                (print "none of them worked"))))))))
 
 ;; check the board state and return name of winner
 ;; false otherwise
--- test/tictactoe/logic_test.clj
+++ test/tictactoe/logic_test.clj
@@ -2,25 +2,29 @@
   (:require [clojure.test :refer :all] 
       [tictactoe.logic :refer :all]))
 
-(deftest next-move-insert
-  (testing "Next move is inserted into board state"
-    (compute-next-move "x" 8)
-    (let [last-move (last board-state)]
-      (is (= "x" (last-move 8))))))
-
 (deftest move-validity
-  (testing "Throws an Exception if a previously played square is played again"
-    (is (thrown? Exception (is-valid-move? "x" 8) 0)))
-  (testing "Throws an Exception if two x-es in a row are played"
-    (is (thrown? Exception (is-valid-move? "x" 4))))
-  (testing "Rejects a move square greater than 8"
-    (is (thrown? AssertionError (is-valid-move? "x" 9))))
-  (testing "Rejects move square less than 0"
-    (is (thrown? AssertionError (is-valid-move? "x" -1)))))
+  (let [board ["" "" "" "" "" "" "" "" "x"]]
+    (testing "Throws an Exception if a previously played square is played again"
+      (is (thrown? Exception (is-valid-move? "x" 8 board) 0)))
+    (testing "Throws an Exception if two x-es in a row are played"
+      (is (thrown? Exception (is-valid-move? "x" 4 board))))
+    (testing "Rejects a move square greater than 8"
+      (is (thrown? AssertionError (is-valid-move? "x" 9 board))))
+    (testing "Rejects move square less than 0"
+      (is (thrown? AssertionError (is-valid-move? "x" -1 board))))))
 
 (deftest move-occurrence-counts
   (testing "Returns 0 if no occurrence of moves in the state"
     (is (= 0 (move-count "y" board-state))))
   (testing "Returns 2 if 2 occurrences of 'x' in the state"
     (is (= 2 (move-count "x" ["x" "o" "o" "" "" "x" "" ""])))))
+
+(deftest correct-move-played
+  (testing "Plays center square if it isn't played"
+    (let [board [(vec (repeat 9 ""))]]
+      (is (= ["" "" "" "x" "o" "" "" "" ""] (compute-next-move "x" 3 board)))))
+  (testing "plays a corner square if center square is played")
+    (let [board [["" "" "" "x" "o" "" "" "" ""]]]
+      (is (= ["o" "x" "" "x" "o" "" "" "" ""] (compute-next-move "x" 1 board) ))
+      (is (= ["o" "x" "o" "x" "o" "" "" "x" ""] (compute-next-move "x" 7 [["o" "x" "" "x" "o" "" "" "" ""]])))))
 
